0ef9b7b0adffade7dab9d16efbead9c8d333843b
Faysal Ahmed <faysal@tikweb.dk>
2023-07-20 10:30:33 +0600

    update

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,16 +1,68 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
+    articles = Article.all
+     render json: {status: "SUCCESS", message: "Article All Data", data: articles, status: :ok} 
   end
 
   def show
+    article = Article.find(params[:id])
+    # article = Article.find_by(id: params[:id])
+
+  if article
+    render json: {data: article}, state: :ok
+  else
+    render json: {message: "Article could not be found"}, status: :bad_request
+  end
   end
 
   def create
+    article = Article.new(
+          title: arti_params[:title],
+          body: arti_params[:body],
+          author: arti_params[:author]
+      )
+
+      if article.save
+          render json: {status: "SUCCESS", message: "Article created successfully!", data: article}, status: :created
+      else
+            render json: {
+              error: "eror creating..."
+            }
+      end      
+
+          # render json: article.errors, status: :unprocessable_entity
   end
 
   def update
+      article = Article.find(params[:id])
+
+  if article.update!(arti_params)
+    render json: {message: "Article was updated successfully", data: article}, status: :ok
+  else
+    render json: {message: "Article cannot be updated"}, status: :unprocessable_entity
+  end
+
   end
 
   def delete
+      article  = Article.find(params[:id])
+
+  if article.destroy!
+    render json: {message: "Article was deleted successfully"}, status: :ok
+  else
+    render json: {message: "Article does not exist"}, status: :bad_request
+  end
+  end
+
+  private
+  def arti_params
+    params.required(:article).permit([
+      :title,
+      :body,
+      :author
+    ])
+      # params.require(:friend).permit(:name, :location, :email, :twitter, :phone)
+
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
     	resources :articles, only: [:index, :show, :create, :update, :delete]
+    	delete "/articles/:url", to: "articles#destroy", constraints: { url: /[^\/]+/ }, as: :articles_destroy_with_url
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
